6cb14207e2f70f051261269f99cc543c21df4931
1718077 <1718077@CS17077>
2018-04-13 20:41:18 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
